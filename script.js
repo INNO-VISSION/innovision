54afe2790d3e7aba92092a08ae94fd18143d4041
hemanth <hemanthtarget8438@gmail.com>
2025-02-04 20:19:50 +0530

    Remove unnecessary blank lines in script.js for cleaner code

--- script.js
+++ script.js
@@ -210,9 +210,6 @@ const courseWeeks = {
         { week: 1, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-/business%20analytics/week%2001/' },
         { week: 2, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-/business%20analytics/week%2002/' },
         { week: 3, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-//business%20analytics/week%2003/' },
-
-
-
     ],
     python_pro: [
         { week: 2, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-/Joy%20Of%20Computing%20Using%20Python%20Programming%20Assignment/week%2002/' },
