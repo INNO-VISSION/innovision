bec7ca4bbf2d7ee141114e49e75e4154699ae518
hemanth <hemanthtarget8438@gmail.com>
2025-02-25 23:31:43 +0530

    Add week 5 to courseWeeks in script.js and adjust image width and height in style.css for better layout

--- script.js
+++ script.js
@@ -238,7 +238,8 @@ const courseWeeks = {
         { week: 1, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-/business%20analytics/week%2001/' },
         { week: 2, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-/business%20analytics/week%2002/' },
         { week: 3, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-//business%20analytics/week%2003/' },
-        { week: 4, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-//business%20analytics/week%2004/' }
+        { week: 4, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-//business%20analytics/week%2004/' },
+        { week: 5, pdf: 'https://inno-vission.github.io/NPTEL-ANSWERS-2025-JAN-APR-//business%20analytics/week%2005/' }
 
     ],
     python_pro: [
